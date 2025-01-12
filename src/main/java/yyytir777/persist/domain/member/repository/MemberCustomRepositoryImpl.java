fe9e33776ccf3eda7b4723722d124e578e46b556
임원재 <80938176+yyytir777@users.noreply.github.com>
2025-01-13 00:06:52 +0900

    [Feat] : MemberRepository 메서드 querydsl 적용 + test용 SecurityTestConfig 생성

---
+++ src/main/java/yyytir777/persist/domain/member/repository/MemberCustomRepositoryImpl.java
@@ -0,0 +1,26 @@
+package yyytir777.persist.domain.member.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import yyytir777.persist.domain.member.entity.QMember;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberCustomRepositoryImpl implements MemberCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public Optional<String> findReadmeById(String memberId) {
+        QMember member = QMember.member;
+        String result = jpaQueryFactory
+                .select(member.id)
+                .from(member)
+                .where(member.id.eq(memberId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
--- src/main/java/yyytir777/persist/global/config/SecurityTestConfig.java
+++ src/main/java/yyytir777/persist/global/config/SecurityTestConfig.java
@@ -2,6 +2,7 @@ package yyytir777.persist.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile("test")
 public class SecurityTestConfig {
 
     @Bean
