6f660a43b99722555a005106f9ca6a819723a754
Wonjae Lim <yyytir777@gmail.com>
2025-02-06 15:00:03 +0900

    [Feat] : TokenRepository 애노테이션 추가 및 RedisRepositoryTest작성

--- src/main/java/yyytir777/persist/global/jwt/repository/TokenRepository.java
+++ src/main/java/yyytir777/persist/global/jwt/repository/TokenRepository.java
@@ -1,8 +1,10 @@
 package yyytir777.persist.global.jwt.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import yyytir777.persist.global.jwt.RefreshToken;
 
+@Repository
 public interface TokenRepository extends CrudRepository<RefreshToken, Long> {
 
 }
